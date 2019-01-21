9561e6479ad07631cd83312bd125b7b12f67d643
Yannick Thirion <yannick.thirion.yt@gmail.com>
2019-01-21 19:26:12 +0100

    Bug fixed... stupid error.

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ function chooseRandomCategory (categories) {
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
   // return category object with that randomArrayIndex
-  return categories[randomArrayIndex];
+  return categories[randomArrayIndex].short_name;
 }
 
 
